9ec37a5eaf60f03b333ef0b1728d0449a57f4102
LuckCrafter <jeremy@gottlieb.cyou>
2025-10-24 18:29:56 +0200

    rm: pufferpanel part (performance)

--- plugin.go
+++ plugin.go
@@ -2,17 +2,11 @@ package hostredirect
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"net/url"
-	"os"
 	"strings"
 	"time"
 
 	"github.com/go-logr/logr"
-	"github.com/pufferpanel/pufferpanel/v3/models"
 	"github.com/pufferpanel/pufferpanel/v3/oauth2"
 	"github.com/robinbraemer/event"
 
@@ -43,11 +37,7 @@ var Plugin = proxy.Plugin{
 		log := logr.FromContextOrDiscard(ctx)
 
 		log.Info("HostRedirect Initlize...")
-		baseurl := os.Getenv("HOSTREDIRECT_URL")
-		clientId := os.Getenv("HOSTREDIRECT_CLIENTID")
-		clientSecret := os.Getenv("HOSTREDIRECT_CLIENTSECRET")
-		client := NewClient(ctx, baseurl, clientId, clientSecret)
-		event.Subscribe(p.Event(), 0, onPlayerChooseInitialServer(p, log, client))
+		event.Subscribe(p.Event(), 0, onPlayerChooseInitialServer(p, log))
 
 		log.Info("HostRedirect Initlized")
 		return nil
@@ -55,34 +45,18 @@ var Plugin = proxy.Plugin{
 }
 
 // onPlayerChooseInitialServer handles the PlayerChooseInitialServerEvent to redirect players.
-func onPlayerChooseInitialServer(p *proxy.Proxy, log logr.Logger, c *Client) func(*proxy.PlayerChooseInitialServerEvent) {
+func onPlayerChooseInitialServer(p *proxy.Proxy, log logr.Logger) func(*proxy.PlayerChooseInitialServerEvent) {
 	return func(e *proxy.PlayerChooseInitialServerEvent) {
 		// Get the player's connecting host.
 		conn := e.Player().VirtualHost()
 		host := conn.String()
 		host = lite.ClearVirtualHost(host)
 
-		config := c.getConfig(log)
+		serverDomain, _, _ := strings.Cut(host, ".")
 
-		// Check if we have a server mapping for this host.
-		serverName, ok := config.ServerMappings[host]
-		if !ok {
-			msg := fmt.Sprintf("No server mapping for host: %s", host)
-			log.Info(msg)
-			return
-		}
-
-		// Get the registered server by name.
-		var server proxy.RegisteredServer
-		for _, s := range p.Servers() {
-			if s.ServerInfo().Name() == serverName {
-				server = s
-				break
-			}
-		}
-
+		server := p.Server(serverDomain)
 		if server == nil {
-			msg := fmt.Sprintf("Server not found: %s", serverName)
+			msg := fmt.Sprintf("Server not found: %s", server)
 			log.Info(msg)
 			e.Player().Disconnect(&component.Text{Content: msg})
 			return
@@ -90,198 +64,6 @@ func onPlayerChooseInitialServer(p *proxy.Proxy, log logr.Logger, c *Client) fun
 
 		// Redirect the player to the appropriate server.
 		e.SetInitialServer(server)
-		log.Info("Redirecting player", "username", e.Player().Username(), "server", serverName)
+		log.Info("Redirecting player", "username", e.Player().Username(), "server", server)
 	}
-}
-
-func NewClient(ctx context.Context, baseUrl, clientId, clientSecret string) *Client {
-	return &Client{
-		ctx:          ctx,
-		baseUrl:      baseUrl,
-		clientId:     clientId,
-		clientSecret: clientSecret,
-	}
-}
-
-func (c *Client) RequestToken() (*oauth2.TokenResponse, error) {
-	form := url.Values{
-		"client_id":     {c.clientId},
-		"client_secret": {c.clientSecret},
-		"grant_type":    {"client_credentials"},
-	}
-
-	req, err := http.NewRequestWithContext(c.ctx, http.MethodPost, fmt.Sprintf("%s/oauth2/token", c.baseUrl), strings.NewReader(form.Encode()))
-	if err != nil {
-		return nil, fmt.Errorf("creating token request: %w", err)
-	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("executing token request: %w", err)
-	}
-	defer func() { _ = resp.Body.Close() }()
-
-	var token oauth2.TokenResponse
-	if err := json.NewDecoder(resp.Body).Decode(&token); err != nil {
-		return nil, fmt.Errorf("decoding token response: %w", err)
-	}
-
-	return &token, nil
-}
-
-func (c *Client) RenewToken() (*oauth2.TokenResponse, error) {
-	if c.token != nil && time.Now().Before(c.tokenExpiresAt) {
-		return c.token, nil
-	}
-
-	tok, err := c.RequestToken()
-	if err != nil {
-		return nil, err
-	}
-
-	c.token = tok
-	c.tokenExpiresAt = time.Now().Add(time.Second * time.Duration(tok.ExpiresIn))
-
-	return c.token, nil
-}
-
-func (c *Client) ServerSearch() (*models.ServerSearchResponse, error) {
-	return c.serverSearch(1)
-}
-
-func (c *Client) serverSearch(page uint) (*models.ServerSearchResponse, error) {
-	token, err := c.RenewToken()
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequestWithContext(c.ctx, http.MethodGet, fmt.Sprintf("%s/api/servers?page=%d&limit=100", c.baseUrl, page), nil)
-	if err != nil {
-		return nil, fmt.Errorf("creating server search request: %w", err)
-	}
-	req.Header.Set("Authorization", fmt.Sprintf("%s %s", token.TokenType, token.AccessToken))
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("executing server search request: %w", err)
-	}
-	defer func() { _ = resp.Body.Close() }()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	var search *models.ServerSearchResponse
-	if err := json.Unmarshal(body, &search); err != nil {
-		return nil, err
-	}
-
-	if int64(search.Metadata.Paging.Size*page) < search.Metadata.Paging.Total {
-		var extraSearch *models.ServerSearchResponse
-		extraSearch, err = c.serverSearch(page + 1)
-		search.Servers = append(search.Servers, extraSearch.Servers...)
-	}
-
-	return search, nil
-}
-
-type Value struct {
-	Value string `json:"value"`
-}
-
-type Option struct {
-	Domain     Value `json:"ip"`
-	ServerName Value `json:"gate.hostredirect.servername"`
-}
-
-type Data struct {
-	Data Option `json:"data"`
-}
-
-func (c *Client) ServerData(serverID string) (*Data, error) {
-	token, err := c.RenewToken()
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequestWithContext(c.ctx, http.MethodGet, fmt.Sprintf("%s/api/servers/%s/definition", c.baseUrl, serverID), nil)
-	if err != nil {
-		return nil, fmt.Errorf("creating server search request: %w", err)
-	}
-	req.Header.Set("Authorization", fmt.Sprintf("%s %s", token.TokenType, token.AccessToken))
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("executing server search request: %w", err)
-	}
-	defer func() { _ = resp.Body.Close() }()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	var data Data
-	if err := json.Unmarshal(body, &data); err != nil {
-		return nil, err
-	}
-	if data.Data.Domain.Value == "" {
-		return nil, fmt.Errorf("Empty")
-	}
-
-	return &data, nil
-}
-
-func (c *Client) getServer(id, path string) ([]byte, error) {
-	token, err := c.RenewToken()
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequestWithContext(c.ctx, http.MethodGet, fmt.Sprintf("%s/api/servers/%s/file/%s", c.baseUrl, id, path), nil)
-	if err != nil {
-		return nil, fmt.Errorf("creating read server file request: %w", err)
-	}
-	req.Header.Set("Authorization", fmt.Sprintf("%s %s", token.TokenType, token.AccessToken))
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("executing read server file request: %w", err)
-	}
-	defer func() { _ = resp.Body.Close() }()
-
-	return io.ReadAll(resp.Body)
-}
-
-func (c *Client) getConfig(log logr.Logger) Config {
-	var config Config
-	config.ServerMappings = map[string]string{}
-	search, err := c.ServerSearch()
-	if err != nil {
-		return config // maybe fallback file
-	}
-	for _, server := range search.Servers {
-		data, err := c.ServerData(server.Identifier)
-		if err != nil {
-			log.Error(server.Identifier)
-			continue
-		}
-		log.Info(server.Identifier)
-		if len(strings.TrimSpace(data.Data.ServerName.Value)) == 0 || len(strings.TrimSpace(data.Data.Domain.Value)) == 0 {
-			log.Info("Error: Empty Values")
-			log.Info(fmt.Sprintf("Error: map[%s]=%s", data.Data.Domain.Value, data.Data.ServerName.Value))
-			continue
-		}
-		if data.Data.Domain.Value == strings.Split(data.Data.Domain.Value, ":")[0] {
-			config.ServerMappings[data.Data.Domain.Value+":25565"] = data.Data.ServerName.Value
-			log.Info(fmt.Sprintf("map[%s:25565]=%s", data.Data.Domain.Value, data.Data.ServerName.Value))
-		} else {
-			config.ServerMappings[data.Data.Domain.Value] = data.Data.ServerName.Value
-			log.Info(fmt.Sprintf("map[%s]=%s", data.Data.Domain.Value, data.Data.ServerName.Value))
-		}
-	}
-	//    fmt.Println(string(config))
-	return config
 }
